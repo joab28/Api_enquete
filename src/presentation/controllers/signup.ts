8d461a5382ced63ff4b21a51bc32b48f7de6d003
joab wendson <joab.wendson61@gmail.com>
2022-04-10 11:47:57 -0300

    refactor: simplifica codigo

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { MissingParamsError } from '../error/missing-params-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   hundle (httpRequest: httpRequest): httpResponse | undefined {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamsError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamsError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field))
+      }
     }
   }
 }
