4ccace984b22a0bdfd4164abf7c402afc94e4b3f
joab wendson <joab.wendson61@gmail.com>
2022-04-10 11:43:29 -0300

    refactor: move codigo duplicado para metodo helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,13 @@
 import { httpRequest, httpResponse } from '../protocols/http'
 import { MissingParamsError } from '../error/missing-params-error'
+import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   hundle (httpRequest: httpRequest): httpResponse | undefined {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamsError('name')
-      }
+      return badRequest(new MissingParamsError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamsError('email')
-      }
+      return badRequest(new MissingParamsError('email'))
     }
   }
 }
