939699c77cf0e6034d3fe917ec9bbdf4505b918f
joab wendson <joab.wendson61@gmail.com>
2022-05-08 11:33:16 -0300

    feat: mongodb in jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
