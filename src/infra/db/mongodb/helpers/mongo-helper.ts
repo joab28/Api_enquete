16d752fdef2adddc72b1e5a6b65e71c6bdedfb67
joab wendson <joab.wendson61@gmail.com>
2022-05-09 20:07:33 -0300

    feat: ensure accountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,15 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+    })
+  },
+  async disconnect () {
+    await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
